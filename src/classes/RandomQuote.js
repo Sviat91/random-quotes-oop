4d522582088f24c7b159dc3aec9881c731a7d106
Sviat91 <svyatoslav.upirov@gmail.com>
2026-03-29 15:30:42 +0200

    Refactor quote generation by restructuring classes and moving utility functions

--- src/classes/RandomQuote.js
+++ src/classes/RandomQuote.js
@@ -1,10 +1,10 @@
-import quotes from './data/quotes.js';
-import { generateRandomInt } from './utils/math.js';
+import quotes from '../data/quotes.js';
+import MathUtils from '../utils/MathUtils.js';
 import Quote from './Quote.js';
 
 class RandomQuote {
   static getRandomQuote() {
-    const randomIndex = generateRandomInt(quotes.length);
+    const randomIndex = MathUtils.generateRandomInt(quotes.length);
     const { id, text, author } = quotes[randomIndex];
     return new Quote(id, text, author);
   }
--- src/index.js
+++ src/index.js
@@ -1,27 +1,3 @@
-import RandomQuote from './RandomQuote.js';
-
-class RandomQuotesApp {
-  constructor() {
-    this.randomQuoteBtn = document.getElementById('generate-btn');
-    this.quoteTextElement = document.getElementById('quote-text');
-    this.authorElement = document.getElementById('author');
-    this.currentQuote = null;
-    this.init();
-  }
-  displayQuote() {
-    const { text, author } = this.currentQuote;
-    this.quoteTextElement.textContent = `"${text}"`;
-    this.authorElement.textContent = author;
-  }
-  getRandomQuote() {
-    const randomQuote = RandomQuote.getRandomQuote();
-    this.currentQuote = randomQuote;
-    this.displayQuote(randomQuote);
-  }
-
-  init() {
-    this.randomQuoteBtn.addEventListener('click', () => this.getRandomQuote());
-  }
-}
+import RandomQuotesApp from './classes/RandomQuotesApp.js';
 
 new RandomQuotesApp();
